a6c8d1bc62e6661b23798734a872218f6b4eee5c
antonlinevych <anton@linevich.net>
2024-11-16 18:21:50 -0600

    Disable auto-suspend for gnome due to graphics glitches w/ GDM

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -50,6 +50,7 @@
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.autoSuspend = false;
   services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
