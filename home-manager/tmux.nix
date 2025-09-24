b2a670df6b23640e18d977f6637ea9c95bbee250
antonlinevych <anton@linevich.net>
2025-09-24 05:07:35 -0500

    Tmux: keybinds, theme

--- home-manager/tmux.nix
+++ home-manager/tmux.nix
@@ -14,13 +14,27 @@ in
     plugins = with pkgs.tmuxPlugins; [
       sensible
       vim-tmux-navigator
+      catppuccin
+      yank
     ];
 
     extraConfig = ''
       # Keybinding
       bind | split-window -h
-      bind v split-window -v
       bind r source-file ${conf} \; display "Reloaded!"
+                  
+      # Theme
+      set -g @catppuccin_flavour '${config.catppuccin.flavor}'
+
+      # Sensible indexing for windows and panes
+      set -g base-index 1
+      set -g pane-base-index 1
+       
+      # TODO: copy mode enter is Leader + [ which is cumbersome, remap to something 
+      set-window-option -g mode-keys vi 
+      bind-key -T copy-mode-vi v send-keys -X begin-selection
+      bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+      bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
     '';
   };
 }
